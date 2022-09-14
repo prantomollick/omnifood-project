bf4e6e8dae46c21bb6dd73ff2ac0c4fb9bfa1a36
pranto mollick <prantocse01@gmail.com>
2022-09-14 17:59:38 +0600

    implementing a sticky navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+console.log(sectionHeroEl);
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 // fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
